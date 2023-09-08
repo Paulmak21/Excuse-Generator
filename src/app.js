2ec1772ee1b9aa80cfc4d7e93ab9a8a5935bf1c2
Paul <140537985+Paulmak21@users.noreply.github.com>
2023-09-08 12:41:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector('#btn').addEventListener("click",() => {
+    document.querySelector('#the-excuse').innerHtml = generateExcuse();
+  });
+  console.log('Hello Rigo from the console!');
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateExcuse = () => {
+    
+    let pronoun = ['A', 'The'];
+    let subject = ['jogger', 'racoon', 'dog', 'driver', 'comedian', 'pincone'];
+    let action = ['took my', 'threw my', 'yelled at my', 'stole my', 'bit my'];
+    let possetion = ['homework', 'toe', 'car', 'shoe'];
+    let where = ['on street', 'in my house', 'in my driveway'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let proIndx = Math.floor(Math.random() * pronoun.length);
+    let subIndx = Math.floor(Math.random() * subject.length);
+    let actionIndx = Math.floor(Math.random() * action.length);
+    let possetionIndex = Math.floor(Math.random() * possetion.length);
+    let whereIndex = Math.floor(Math.random() * where.length);
+
+    return pronoun[proIndx] + ' ' + subject[subIndx] + ' ' + action[actionIndx] + ' ' + possetion[possetionIndex] + ' ' + where[whereIndex];
 };
